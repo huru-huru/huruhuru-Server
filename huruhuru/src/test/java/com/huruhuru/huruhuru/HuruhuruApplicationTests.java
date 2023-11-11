c65350597ad85ba64838871d02bd655de9f43c47
soeun <separk314@naver.com>
2023-11-12 00:40:01 +0900

    chore: Springboot 기본 프로젝트 세팅 #1

---
+++ huruhuru/src/test/java/com/huruhuru/huruhuru/HuruhuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huruhuru.huruhuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuruhuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
