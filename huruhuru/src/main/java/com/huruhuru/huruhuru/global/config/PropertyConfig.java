8ff2d635c601fb39cca7ff132c4b8961d0c1a6ed
soeun <separk314@naver.com>
2023-11-12 01:04:24 +0900

    chore: 프로젝트 디렉토리 및 기본 설정 파일 세팅 #1

---
+++ huruhuru/src/main/java/com/huruhuru/huruhuru/global/config/PropertyConfig.java
@@ -0,0 +1,9 @@
+package com.huruhuru.huruhuru.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/.env")
+public class PropertyConfig {
+}
