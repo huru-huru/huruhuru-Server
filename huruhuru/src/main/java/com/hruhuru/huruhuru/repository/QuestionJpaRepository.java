7160795c6253bbd22a955d4e0e8b07db0a12200b
jinyoungpark00 <doghong00@naver.com>
2023-11-14 13:07:06 +0900

    feat: test 완료시 testCount 1 증가 API 구현

--- huruhuru/src/main/java/com/hruhuru/huruhuru/repository/QuestionJpaRepository.java
+++ huruhuru/src/main/java/com/hruhuru/huruhuru/repository/QuestionJpaRepository.java
@@ -2,12 +2,27 @@ package com.hruhuru.huruhuru.repository;
 
 import com.hruhuru.huruhuru.domain.entity.QuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface QuestionJpaRepository extends JpaRepository<QuestionEntity, Long> {
 
+    // 모든 testCount의 값 합쳐서 반환
     @Query("SELECT SUM(q.testCount) FROM QuestionEntity q")
     Optional<Long> getTotalTestCount();
+
+    // id로 question 찾아서 testCount 반환
+    @Query("SELECT SUM(q.testCount) FROM QuestionEntity q WHERE q.id = :questionId")
+    Optional<Long> getTestCountByCategoryAndTheme(@Param("questionId") Long questionId);
+
+    // id로 question 찾아서 testCount 1 증가
+    @Transactional
+    @Modifying
+    @Query("UPDATE QuestionEntity q SET q.testCount = q.testCount + 1 WHERE q.id = :questionId")
+    void plusTotalTestCount(@Param("questionId") Long questionId);
 }
