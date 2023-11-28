2c9d4cbd36d06281a002baae941df07218193de4
eunsu02 <regulus_leo08@naver.com>
2023-11-29 01:52:05 +0900

    [fix] cors 오류

--- huruhuru/src/main/java/com/huruhuru/huruhuru/global/config/SecurityConfig.java
+++ huruhuru/src/main/java/com/huruhuru/huruhuru/global/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+import java.util.List;
 
 
 @Configuration
@@ -61,7 +61,9 @@ public class SecurityConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("https://huruhuru.netlify.app", "http://localhost:3000")
                         .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedMethods("*");
+
             }
         };
     }
