afa4898b3314fa7c416e97979d902876e84b7290
eunsu02 <regulus_leo08@naver.com>
2023-11-29 00:23:13 +0900

    [feat] 테마별 점수 가져오는 기능 추가

--- huruhuru/src/main/java/com/huruhuru/huruhuru/repository/ScoreJpaRepository.java
+++ huruhuru/src/main/java/com/huruhuru/huruhuru/repository/ScoreJpaRepository.java
@@ -5,8 +5,10 @@ import com.huruhuru.huruhuru.domain.entity.QuestionEntity;
 import com.huruhuru.huruhuru.domain.entity.ScoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ScoreJpaRepository extends JpaRepository<ScoreEntity, Long> {
     Optional<ScoreEntity> findByMemberAndTheme(MemberEntity member, Long theme);
+    List<ScoreEntity> findByMemberId(Long memberId);
 }
